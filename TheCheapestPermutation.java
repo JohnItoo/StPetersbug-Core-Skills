7b9b63f8cb947f9ad68094cda1c2b17ddf8583f9
JohnOhue <itoohue@gmail.com>
2020-06-22 14:00:06 +0100

    2020-06-22

--- TheCheapestPermutation.java
+++ TheCheapestPermutation.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class TheCheapestPermutation {
     static int N;
+
     public static void main(String[] arg) {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
@@ -16,10 +17,9 @@ public class TheCheapestPermutation {
                 a[i][j] = in.nextInt();
 
         int[] result = new int[n];
-
-        permute(0,n, result);
-
-        // your code
+        int[] tent = new int[n];
+        
+        permute(0, n, result, tent, a);
 
         for (int i = 0; i < result.length; ++i) {
             if (i != 0) out.print(' ');
@@ -30,22 +30,28 @@ public class TheCheapestPermutation {
         in.close();
         out.close();
     }
+
     static boolean[] done = new boolean[10];
-    static int sum = 
-    static void permute(int k, int n, int[] result) {
-        if(k < n) {
-            for(int i = 0; i < n; i++) {
-                if(done[i]) continue;
+    static int sum = Integer.MAX_VALUE;
+
+    static void permute(int k, int n, int[] result, int[] tent, int[][] a) {
+        if (k < n) {
+            for (int i = 0; i < n; i++) {
+                if (done[i]) continue;
                 done[i] = true;
-                result[k] = i+1;
-                permute(k+1, n, result);
+                tent[k] = i + 1;
+                permute(k + 1, n, result, tent, a);
                 done[i] = false;
             }
         } else {
-            for(int element : result) {
-                System.out.print(element + " ");
+            int current = 0;
+            for (int i = 0; i < tent.length - 1; i++) {
+                current += a[tent[i] - 1][tent[i + 1] - 1];
             }
-            System.out.println();
+            if (current < sum) {
+                System.arraycopy(tent, 0, result, 0, tent.length);
+                sum = current;
+            }
         }
     }
 }
