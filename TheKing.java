b09b60305ffc2d6af9d0c991f909f34b08c09cb6
JohnOhue <itoohue@gmail.com>
2020-06-24 11:00:08 +0100

    2020-06-24

---
+++ TheKing.java
@@ -0,0 +1,23 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class TheKing {
+    public static void main(String[] arg) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int r, c;
+        r = in.nextInt();
+        c = in.nextInt();
+
+        int result = 0;
+
+        // your code
+        placei, j)
+
+        out.println(result);
+
+        in.close();
+        out.close();
+    }
+}
