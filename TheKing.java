255afc5983497a02fa6d84b6bfed3520414f7184
JohnOhue <itoohue@gmail.com>
2020-06-25 10:00:13 +0100

    2020-06-25

--- TheKing.java
+++ TheKing.java
@@ -13,14 +13,26 @@ public class TheKing {
         int result = 0;
 
         // your code
-        place(0, 0, r,c, board);
+        place(0, 0, r,c, board,0);
 
         out.println(result);
 
         in.close();
         out.close();
     }
-    static int place(int r, int c, int rows, int cols, int[][] board, int kings) {
-        if(r == rows && )
+    static int ans = 0;
+    static void place(int r, int c, int rows, int cols, int[][] board, int kings) {
+        if(r == rows && c == cols) {
+            ans = Math.max(ans, kings);
+            return;
+        }
+        int[][] spaces = { {0,1}, {1,1} , {1,0}, {1,-1}};
+        for(int i = 0; i <4; i++) {
+            int nr = r + spaces[i][0];
+            int nc = r + spaces[i][1];
+            if( nr >= rows || nr < 0 || nc >= cols || nc <0) continue;
+        }
+
+
     }
 }
