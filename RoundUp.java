bbbc07f203732024b9fb837c79a8e72578cc4e45
JohnOhue <itoohue@gmail.com>
2020-06-30 18:00:10 +0100

    2020-06-30

--- RoundUp.java
+++ RoundUp.java
@@ -10,18 +10,14 @@ public class RoundUp {
         long y;
         x = in.nextLong();
         y = in.nextLong();
-        long z = 0;
-//        if(y * z >= x) {
-//            out.println(0);
-//        } else {
-//            z = 1;
-//            while(y*z < x) {
-//                z += 1;
+        long z = (long) -10e9;
+
+            while(y*z <x) {
+                z += 1;
 //                out.println(z);
-//            }
-//            out.println(z);
-//        }
-        out.println(y*z);
+            }
+            out.println(z);
+
 
         in.close();
         out.close();
