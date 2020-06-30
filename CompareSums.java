ce5a12d2705d580574aa6b00762d293237c0eb34
JohnOhue <itoohue@gmail.com>
2020-06-30 16:00:19 +0100

    2020-06-30

--- CompareSums.java
+++ CompareSums.java
@@ -8,19 +8,22 @@ public class CompareSums {
 
         int n;
         n = in.nextInt();
-        float[] a = new float[n + 1];
-        float suma = 0f;
-        float sumb = 0f;
+        double[] a = new double[n + 1];
+        double suma = 0.0000000000;
+        double sumb = 0.0000000000f;
         for (int i = 0; i < n; i++) {
-            a[i]= in.nextFloat();
-            suma +=  a[i];
+            a[i]= in.nextDouble();
+            suma += a[i];
+            out.println(suma);
 //            out.println( (a[i] * 1000) + " " + a[i]);
         }
-        float[] b = new float[n + 1];
-
+        double[] b = new double[n + 1];
+         out.println("SUMB");
         for (int i = 0; i < n; i++) {
-            b[i] = in.nextFloat();
-            sumb +=   b[i];
+            b[i] = in.nextDouble();
+            sumb += b[i];
+            out.println(sumb);
+
 //            out.println( (b[i] * 1000) + " " + b[i]);
 
         }
@@ -29,12 +32,13 @@ public class CompareSums {
         out.println("This is suma" + suma);
 //        float btot = (float) b[n]/1000;
 //        float atot = (float) a[n]/1000;
-        if (suma < sumb) {
-            out.println("SUM(A)<SUM(B)");
-        } else if (suma > sumb) {
+        if (suma == sumb) {
+            out.println("abs is" + Math.abs(suma-sumb)  );
+            out.println("SUM(A)=SUM(B)");
+        } else if (suma > sumb + 0.001) {
             out.println("SUM(A)>SUM(B)");
         } else {
-            out.println("SUM(A)=SUM(B)");
+            out.println("SUM(A)<SUM(B)");
         }
 
         in.close();
