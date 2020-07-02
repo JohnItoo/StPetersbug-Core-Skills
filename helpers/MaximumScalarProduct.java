2cc9d3ec84d89c7e38506d344185f4a9caad3bcb
JohnOhue <itoohue@gmail.com>
2020-07-02 10:00:49 +0100

    2020-07-02

---
+++ helpers/MaximumScalarProduct.java
@@ -0,0 +1,20 @@
+package helpers;
+
+public class MaximumScalarProduct {
+    /**
+     * Lemma; Two arrays a and b maximum scalar product.
+     * ai is max in a
+     * and
+     * bj is max in b
+     * let aq be another number in a
+     * and bx be another number in b
+     *  let S = (ai * bj) + (aq + bx);
+     * and S* = (ai * bx) + (aq*bj);
+     * We want to prove that S is greater than S*
+     *
+     * S - S* (should be greater/equal to) >= 0;
+     * (aibj + aqbx )- (aibx +aqbj) >= 0
+     * (aibj -aibx) + (aqbx - aqbj) >= 0
+     *
+     */
+}
