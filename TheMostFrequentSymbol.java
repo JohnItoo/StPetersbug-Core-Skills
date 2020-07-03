d01f1a9c3beb370de149e7c18105a28a8771806b
JohnOhue <itoohue@gmail.com>
2020-07-03 09:00:06 +0100

    2020-07-03

--- TheMostFrequentSymbol.java
+++ TheMostFrequentSymbol.java
@@ -6,9 +6,20 @@ public class TheMostFrequentSymbol {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
 
+        String s;
+        s = in.nextLine();
         int n;
         n = in.nextInt();
-        
+        for(int i = 0; i < n ; i++) {
+            int l,r;
+            l = in.nextInt();
+            r = in.nextInt();
+            if(l == r) {
+                out.println(s.charAt(l-1));
+            } else {
+                
+            }
+        }
 
         in.close();
         out.close();
