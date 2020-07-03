8b8665b4b0813ef97d677b57d8204cfe13ab19eb
JohnOhue <itoohue@gmail.com>
2020-07-03 18:06:07 +0100

    2020-07-03

---
+++ MaximumSubArray.java
@@ -0,0 +1,17 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class MaximumSubArray {
+    public static void main(String[] arg) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n;
+        n = in.nextInt();
+        if(n%2 != 0) n+=1;
+        int
+
+        in.close();
+        out.close();
+    }
+}
