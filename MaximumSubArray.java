b2b33459eb4fb2be5091f8c3606b82ac1dae941d
JohnOhue <itoohue@gmail.com>
2020-07-04 09:00:06 +0100

    2020-07-04

--- MaximumSubArray.java
+++ MaximumSubArray.java
@@ -12,9 +12,34 @@ public class MaximumSubArray {
         while(len < n) {
             len *= 2;
         }
-        int[] segmentTree = new int[2*len];
+        int segLen = 2*len;
+        long[] segmentTree = new long[segLen];
+        int zeros = len - n;
+        int start = (segLen-1) - (len-1);
+        int rd = 0;
+        while(rd < n) {
+            segmentTree[start++] = in.nextLong();
+            rd++;
+        }
+        int z = 0;
+        while(z < zeros) {
+            segmentTree[start++] = 0;
+            z++;
+        }
+
+        out.println();
+        for(int i = 0; i < segLen; i++) {
+            out.print(segmentTree[i] + " ");
+        }
+//        makeSegmentTree((a - 1) -(n-1), n,  segmentTree);
 
         in.close();
         out.close();
     }
+
+//    private static void makeSegmentTree(long left, long times, long[] segmentTree) {
+//        if(start) {
+//
+//        }
+//    }
 }
