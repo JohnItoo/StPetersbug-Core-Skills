ff05950e4b31da374b7d6096ecca6d2e8ce1adec
JohnOhue <itoohue@gmail.com>
2020-07-05 12:00:08 +0100

    2020-07-05

---
+++ YetAnotherSum.java
@@ -0,0 +1,23 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class YetAnotherSum {
+    public static void main(String[] arg) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+        double wholeSum = 0.00;
+        double doubleSum = 0.00;
+        for (int i = 0; i < n; i++) {
+            double x = in.nextDouble();
+            wholeSum += x;
+            doubleSum += (1/x);
+        }
+        out.println(wholeSum + doubleSum);
+
+
+        in.close();
+        out.close();
+    }
+}
