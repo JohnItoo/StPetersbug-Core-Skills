24ca28b70f03e72ea7bc747662d42b6eb1b64ad6
JohnOhue <itoohue@gmail.com>
2020-07-05 02:00:04 +0100

    2020-07-05

---
+++ ManhattanDistance.java
@@ -0,0 +1,20 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ManhattanDistance {
+    public static void main(String[] arg) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+        long a, b;
+        a = in.nextLong();
+        b = in.nextLong();
+        out.println(1 + " " + 1);
+        for(int i = 2; i <= n; i++) {
+            if()
+        }
+        in.close();
+        out.close();
+    }
+}
