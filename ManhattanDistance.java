279ad2456c0e4f6a05254a6b26c30b7ce60f0e16
JohnOhue <itoohue@gmail.com>
2020-07-05 03:00:05 +0100

    2020-07-05

--- ManhattanDistance.java
+++ ManhattanDistance.java
@@ -11,8 +11,46 @@ public class ManhattanDistance {
         a = in.nextLong();
         b = in.nextLong();
         out.println(1 + " " + 1);
-        for(int i = 2; i <= n; i++) {
-            if()
+        long a2 =0 ,b2 = 0;
+        int maxIdx = 1;
+        int minIdx = 1;
+        if(n >= 2) {
+            a2 = in.nextLong();
+            b2 = in.nextLong();
+            if(a2+b2 > a+b) {
+                long tempx, tempy;
+                tempx = a;
+                tempy = b;
+                a = a2;
+                b = b2;
+                a2 = tempx;
+                b2 = tempy;
+                maxIdx = 2;
+                minIdx = 1;
+            } else {
+                maxIdx = 1;
+                minIdx = 2;
+            }
+            out.println(Math.min(maxIdx, minIdx) + " " + Math.max(maxIdx, minIdx));
+        }
+
+        for(int i = 3; i <= n; i++) {
+            long x, y;
+            x = in.nextLong();
+            y = in.nextLong();
+            if(x+y >= a +b) {
+                a2 = a;
+                b2 = b;
+                a = x;
+                b = y;
+                minIdx = maxIdx;
+                maxIdx = i;
+            } else if (x+y >= a2 + b2) {
+                a2 = x;
+                b2 = y;
+                minIdx = i;
+            }
+            out.println(Math.min(maxIdx, minIdx) + " " + Math.max(maxIdx, minIdx));
         }
         in.close();
         out.close();
