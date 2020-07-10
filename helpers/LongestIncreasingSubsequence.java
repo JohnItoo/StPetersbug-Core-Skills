96d5f6447b9c6d5fbf6e6353b2589fdea93e6940
JohnOhue <itoohue@gmail.com>
2020-07-10 14:00:09 +0100

    2020-07-10

--- helpers/LongestIncreasingSubsequence.java
+++ helpers/LongestIncreasingSubsequence.java
@@ -4,7 +4,7 @@ public class LongestIncreasingSubsequence {
   static int[] mem = new int[10005];
   static int[] prev = new int[1005];
     public static void main(String[] args) {
-        int[] arr = {7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3};
+        int[] arr = {7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1};
 //        recursive(arr);
         iterative(arr);
     }
@@ -39,23 +39,30 @@ public class LongestIncreasingSubsequence {
            prev[i] = -1;
            for(int j = 0; j < i; j++) {
                if(arr[j] < arr[i]) {
-                   mem[i] = Math.max(mem[i], mem[j] +1);
-                   prev[i] = j;
+                   if(mem[j]+1 > mem[i]) {
+                       mem[i] = mem[j]+1;
+                       prev[i] = j;
+                   }
                }
            }
        }
        int idx = 0;
        for(int i = 0; i < arr.length; i++) {
+           System.out.print(mem[i] + " ");
            if(mem[i] > res) {
                idx = i;
                res = mem[i];
            }
        }
+       System.out.println();
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(prev[i] + " ");
+        }
        while(prev[idx] != -1) {
-           System.out.println(arr[idx] + " ");
+           System.out.println(idx + " ");
            idx = prev[idx];
        }
-       System.out.println(arr[idx]);
+       System.out.println(idx);
         System.out.println("ITR : The longest Increasing subsequence of the given array is " + res);
     }
 }
