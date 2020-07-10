c22256cc70e5e494b490a1ad23004943b8974e88
JohnOhue <itoohue@gmail.com>
2020-07-10 12:00:05 +0100

    2020-07-10

--- helpers/LongestIncreasingSubsequence.java
+++ helpers/LongestIncreasingSubsequence.java
@@ -3,11 +3,18 @@ package helpers;
 public class LongestIncreasingSubsequence {
   static int[] mem = new int[10005];
     public static void main(String[] args) {
-        int[] arr = {7,2,1,3,}
+        int[] arr = {7,2,1,3,2,5,1,6};
+        for(int i = 0; i < arr.length; i++) {
+            LIS(arr, i);
+        }
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(mem[i] + " ");
+        }
+
     }
 
     private static int LIS(int [] a, int i) {
-        if(mem[i] != 0) return mem[i];
+        if(mem[i] == 0)  mem[i] = 1;
 
         for(int j = 0; j < i ; j++) {
             if(a[j] < a[i]) {
