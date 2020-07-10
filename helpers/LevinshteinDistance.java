5d6d071d52055c4e572ece90a8db9dd504d03794
JohnOhue <itoohue@gmail.com>
2020-07-10 18:00:08 +0100

    2020-07-10

--- helpers/LevinshteinDistance.java
+++ helpers/LevinshteinDistance.java
@@ -1,6 +1,7 @@
 package helpers;
 
 public class LevinshteinDistance {
+    static int[][] ED = new int[10005][10005];
     /**
      * Minimum Edit distance between two strings A and B.
      * is The number of edits to change A -> B.
@@ -14,5 +15,33 @@ public class LevinshteinDistance {
      * if it is match/mismatch all A's prefix till that point must optimally match all B's prefix till that point.
      *
      * ED(i,j) is the minimum edit distance of A[0...i-1] and B[0...j-1]
+     * ED(i,j) { min( ED(i, j-1) + 1, ED(i-1, j) + 1, ED(1-1, j-1) + diff(A(i-1) , B(j-1) };
+     * NB: This diff is 1 if it is a mismatch and 0 if it is a match;
      */
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void editDistance(String a, String b) {
+      for(int i = 0; i <= 100; i ++) {
+          for(int j = 0; j <= 100; j++) {
+               ED[i][j] = Integer.MAX_VALUE;
+          }
+      }
+      for(int i = 0; i < a.length(); i++) {
+          ED[i][0] = i;
+      }
+      for(int j = 0; j < b.length(); j++) {
+          ED[j][0] = j;
+      }
+        for(int i = 1; i <= a.length(); i++) {
+            for(int j = 1; j <= a.length(); j++) {
+                int diff = a.charAt(i-1) == b.charAt(j-1) ? 0 : 1;
+                ED[i][j] = Math.min( Math.min((ED[i-1][j] + 1), (ED[i-1][j-1] + diff)),
+                        Math.min((ED[i-1][j] + 1), (ED[i][j-1] + 1)));
+            }
+        }
+    }
+
 }
