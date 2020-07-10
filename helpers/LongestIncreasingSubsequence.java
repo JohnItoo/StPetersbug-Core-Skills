8e45536d47606e6328e24f2367938f32f74e08df
JohnOhue <itoohue@gmail.com>
2020-07-10 15:00:08 +0100

    2020-07-10

--- helpers/LongestIncreasingSubsequence.java
+++ helpers/LongestIncreasingSubsequence.java
@@ -32,7 +32,54 @@ public class LongestIncreasingSubsequence {
         return mem[i];
     }
 
+    /** Build/ construct optimal solution without
+     * keeping previous
+     *
+     * @param arr
+     */
     private static void iterative(int [] arr) {
+        int res = 1;
+        for(int i = 0; i <arr.length; i++) {
+            mem[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if(arr[j] < arr[i]) {
+                    if(mem[j]+1 > mem[i]) {
+                        mem[i] = mem[j]+1;
+                    }
+                }
+            }
+        }
+        int idx = 0;
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(mem[i] + " ");
+            if(mem[i] > res) {
+                idx = i;
+                res = mem[i];
+            }
+        }
+        System.out.println();
+        System.out.print(idx + " "); // index of highest element in optimal sequence
+
+        int before = idx;
+        idx--;
+
+        while(idx >= 0) {
+
+            if(arr[before] > arr[idx] && mem[before] - 1 == mem[idx] ) {
+                before = idx;
+                System.out.print(idx + " "); // printing out the indexes
+            }
+            idx --;
+        }
+    }
+
+    /**
+     * Trying to build the optimal solution,
+     * Here we save the previous element to our current element of our optimal solution in an array
+     * prev.
+     * @param arr
+     */
+    private static void iterativeWithPrev(int [] arr) {
         int res = 1;
        for(int i = 0; i <arr.length; i++) {
            mem[i] = 1;
@@ -41,7 +88,7 @@ public class LongestIncreasingSubsequence {
                if(arr[j] < arr[i]) {
                    if(mem[j]+1 > mem[i]) {
                        mem[i] = mem[j]+1;
-                       prev[i] = j;
+                       prev[i] = j; // Updating the previous element in our sequence that ends at i.
                    }
                }
            }
