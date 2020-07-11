6282f413b46d8b3a740546d985dc727076089af0
JohnOhue <itoohue@gmail.com>
2020-07-12 00:00:06 +0100

    2020-07-12

--- ChainMatrixMultiplication.java
+++ ChainMatrixMultiplication.java
@@ -7,23 +7,31 @@ public class ChainMatrixMultiplication {
     public static void main(String[] arg) {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-      int n = in.nextInt();
-      int[] a = new int[n];
-      for(int i = 0; i <n; i++) {
-          a[i] = in.nextInt();
-      }
-      
-      int[][] dp = new int[10000][10000];
-      for(int s = 2; s < n; s++) {
-          for(int i = 0; i <= n-s; i++) {
-              int j = i + s;
-              for(int k = i+ 1; k < j ; k++) {
-                  dp[i][j] = Math.min(dp[i][j],
-                          dp[i][k] + dp[k][j] + a[i]*a[j]*a[k]);
-              }
-          }
-      }
-      out.println(dp[0][n]);
+        int n = in.nextInt();
+        int[] a = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            a[i] = in.nextInt();
+        }
+        long[][] dp = new long[n + 1][n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            dp[i][i + 1] = 0;
+        }
+        for (int s = 2; s < n + 1; s++) {
+            for (int i = 0; i < n - s + 1; i++) {
+                int j = i + s;
+                for (int k = i + 1; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j],
+                            dp[i][k] + dp[k][j] + (a[i] * a[j] * a[k]));
+                }
+            }
+        }
+        out.println(dp[0][n]);
         in.close();
         out.close();
     }
